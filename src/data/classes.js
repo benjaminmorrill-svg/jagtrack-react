07b3d5e3b38598941824cb4bf3d3b4f6216cce05
Benjamin Morrill <morrillb@JEFFERSON.LOCAL>
2025-11-20 09:34:46 -0700

    Checkpoint 2: Components and props with responsive grid

--- src/data/classes.js
+++ src/data/classes.js
@@ -2,36 +2,36 @@ export const classes = [
  {
    id: 1,
    block: 'A Block',
-   name: 'English Literature',
+   name: 'JavaScript Programming',
    time: '8:25 AM - 9:55 AM',
-   description: 'Explore classic and contemporary literature while developing critical reading and writing skills.',
+   description: 'Learn to build modern, responsive websites and interactive web applications using React.',
    canvasUrl: '#',
    color: '#e03131'
  },
  {
    id: 2,
    block: 'B Block',
-   name: 'JavaScript Programming',
+   name: 'Algebra II Honors',
    time: '10:00 AM - 11:30 AM',
-   description: 'Learn to build modern, responsive websites and interactive web applications using React.',
+   description: 'Master advanced algebraic concepts including functions, polynomials, exponentials, and logarithms.',
    canvasUrl: 'https://jajags.instructure.com/courses/3439',
    color: '#1a5490'
  },
  {
    id: 3,
    block: 'C Block',
-   name: 'Algebra II',
+   name: 'College US History (HIS1220)',
    time: '12:15 AM - 1:45 PM',
-   description: 'Master advanced algebraic concepts including functions, polynomials, exponentials, and logarithms.',
+   description: 'Explore US History since the Civil War, covering topics ranging from the Gilded Age to 9/11.',
    canvasUrl: '#',
    color: '#2f9e44'
  },
  {
    id: 4,
    block: 'D Block',
-   name: 'Biology',
+   name: '2D Arts',
    time: '1:50 PM - 3:20 PM',
-   description: 'Investigate living systems from cells to ecosystems through hands-on lab work.',
+   description: 'Practice the elements and principles of deisgn and composition through various 2D mediums.',
    canvasUrl: '#',
    color: '#f08c00'
  }
